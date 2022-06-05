d92e025b1c93113cb7998c78aff9f916a6d22390
Soumya Banerjee <banerjeesoumya15@gmail.com>
2022-06-06 01:24:38 +0530

    Third question

--- Case Study 1 - Danny's Diner/ms-sql/query.sql
+++ Case Study 1 - Danny's Diner/ms-sql/query.sql
@@ -12,3 +12,13 @@ SELECT s.customer_id, COUNT(DISTINCT s.order_date) AS total_days
 FROM dannys_diner.sales s
 GROUP BY s.customer_id
 ORDER BY 1
+
+-- 3. What was the first item from the menu purchased by each customer?
+
+SELECT s1.customer_id, s2.product_id, s1.first_order
+FROM
+(SELECT customer_id, MIN(order_date) AS first_order
+FROM dannys_diner.sales
+GROUP BY customer_id) s1 
+INNER JOIN dannys_diner.sales s2 ON s1.customer_id=s2.customer_id AND s1.first_order=s2.order_date
+;
